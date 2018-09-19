66c3c36244268f4af87209a42ffbe2c904447266
高天泽 <535262213@qq.com>
2018-09-20 02:41:52 +0800

    change

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,57 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        id: '0001',
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票新城店'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        id: '0002',
+        title: '学生票',
+        children: [{
+          title: '学生三馆联票'
+        }, {
+          title: '学生五馆联票'
+        }]
+      }, {
+        id: '0003',
+        title: '儿童票',
+        children: [{
+          title: '儿童三馆联票'
+        }, {
+          title: '儿童五馆联票'
+        }]
+      }, {
+        id: '0004',
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
