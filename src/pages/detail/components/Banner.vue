1a3ed2f1f353f216283fb86516117d9092c8e201
高天泽 <535262213@qq.com>
2018-09-19 19:33:09 +0800

    change

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">
+          大连圣亚海洋世界(AAAA景区)
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe796;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"
+    >
+    </common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg', 'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    position: relative
+    .banner-img
+      width: 100%
+    .banner-info
+      position: absolute
+      bottom: 0
+      left: 0
+      right: 0
+      color: #fff
+      display: flex
+      line-height .6rem
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        line-height: .32rem
+        height: .32rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0 , .6)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
